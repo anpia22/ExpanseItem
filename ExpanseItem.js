ecec1b8f4b21ed71e8340c9174d284f5d419bd1f
Anup Ashtankar <122741245+anpia22@users.noreply.github.com>
2023-05-08 14:48:12 +0530

    ExpanseItem

---
+++ ExpanseItem.js
@@ -0,0 +1,13 @@
+function ExpanseItem(){
+    return (
+        <div>
+            <h2>Expanse Item!</h2>
+            <div>Food Rs 10</div>
+            <div>Petrol Rs 100</div>
+            <div>Movie Rs 200</div>
+        </div>
+    
+    )
+}
+
+export default ExpanseItem;
